a6f950706072fcc55ef5aa8a71bdd619279443cb
Michael Lawrence <lawremi@iastate.edu>
2013-07-14 00:46:05 +0000

    finally remove the junk left over from Jeremiah
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/VariantTools@78428 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/plotTiTv.R
+++
@@ -1,5 +0,0 @@
-plotTiTv <- function(gr, main = "TiTv"){
-  titv <- table(as.character(values(gr)$ref), as.character(values(gr)$alt))
-  col = c("firebrick3", "dodgerblue3", "forestgreen", "goldenrod3")
-  mosaicplot(titv, col = col, main = main)
-}
