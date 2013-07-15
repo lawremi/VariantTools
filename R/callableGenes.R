f635ef26c998ecf2d91f97ead6d2324a03615a3b
Michael Lawrence <lawremi@iastate.edu>
2013-07-15 23:57:40 +0000

    callableFraction now a generic with methods for GRangesList and TranscriptDb
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/VariantTools@78535 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/callableGenes.R
+++ R/callableGenes.R
@@ -1,9 +1,19 @@
-callableFraction <- function(tx, reads, ...) {
-  callable <- callCallable(reads, ...)
-  seqlevels(tx) <- names(cov)
-  tx_gr <- unlist(tx, use.names = FALSE)
-  tx_ranges <- split(ranges(tx_gr),
-                     seqnames(tx_gr), names(cov))
-  rowsum(unlist(viewSums(Views(tx_ranges, callable)), use.names = FALSE),
-         names(tx)[togroup(tx)]) / sum(width(tx))[togroup(tx)]
-}
+setGeneric("callableFraction",
+           function(tx, reads, ...) standardGeneric("callableFraction"))
+
+setMethod("callableFraction", c("GRangesList", "ANY"),
+          function(tx, reads, ...) {
+            callable <- callCallable(reads, ...)
+            seqlevels(tx) <- names(cov)
+            tx_gr <- unlist(tx, use.names = FALSE)
+            tx_ranges <- split(ranges(tx_gr), seqnames(tx_gr))
+            exon_sums <- unsplit(viewSums(Views(callable, tx_ranges)),
+                                 seqnames(tx_gr))
+            tx_sums <- rowsum(exon_sums, names(tx)[togroup(tx)])
+            tx_sums / sum(width(tx))
+          })
+
+setMethod("callableFraction", c("TranscriptDb", "ANY"),
+          function(tx, reads, ...) {
+            callGeneric(exonsBy(tx), reads, ...)
+          })
