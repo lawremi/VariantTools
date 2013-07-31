69e97a92cf9deda069a1356b656b81ec847de6b7
Michael Lawrence <lawremi@iastate.edu>
2013-07-31 02:56:15 +0000

    spell variantGRanges2VRanges correctly
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/VariantTools@79141 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/cgpGr2Vcf.R
+++ R/cgpGr2Vcf.R
@@ -5,7 +5,7 @@ variantGR2Vcf <- function(x, sample.id, project = NULL,
                           genome = unique(GenomicRanges::genome(x)))
 {
   variantGRangesIsDeprecated("variantGR2Vcf")
-  vr <- variantGRangesToVRanges(x)
+  vr <- variantGRanges2VRanges(x)
   sampleNames(vr) <- sample.id
   genome(vr) <- genome
   metadata(vr)$project <- project
