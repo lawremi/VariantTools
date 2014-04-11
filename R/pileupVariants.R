ea18d5a4379cc90002d4dc8b53818256d0ec6210
Michael Lawrence <lawremi@iastate.edu>
2014-04-11 20:15:21 +0000

    fixes for pileupVariants
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/VariantTools@88832 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/pileupVariants.R
+++ R/pileupVariants.R
@@ -7,7 +7,7 @@
 
 pileupVariants <- function(bams, genome, param = PileupParam(),
                            minAltDepth = 1L, baseOnly = TRUE,
-                           BPPARAM = defaultBPPARAM(), ...)
+                           BPPARAM = defaultBPPARAM())
 {
   pileupFiles <- PileupFiles(bams)
   plpWhat(param) <- union(plpWhat(param), "seq")
@@ -19,8 +19,10 @@ pileupVariants <- function(bams, genome, param = PileupParam(),
     sn <- Rle(names(x$seqnames), x$seqnames)
     pos.gr <- GRanges(sn, IRanges(x$pos, width=1))
     seq.gr <- pos.gr
-    seqnameStyle(seq.gr) <- seqnameStyle(genome)
-    ref.pos <- getSeq(genome, seq.gr, as.character=TRUE)
+    if (!all(seqlevels(seq.gr) %in% seqlevels(genome))) {
+      seqlevelsStyle(seq.gr) <- seqlevelsStyle(genome)
+    }
+    ref.pos <- as.character(getSeq(genome, seq.gr))
     ref <- rep(ref.pos, each=nrow(tally)*ncol(tally))
     bases <- rep(rownames(tally), ncol(tally) * dim(tally)[3])
     is.ref <- ref == bases
@@ -31,7 +33,7 @@ pileupVariants <- function(bams, genome, param = PileupParam(),
     vr <- VRanges(rep(sn, each=per.pos),
                   IRanges(rep(x$pos, each=per.pos), width=1),
                   alt = bases[!is.ref],
-                  ref = rep(ref[is.ref], each=per.pos),
+                  ref = rep(ref[is.ref], each=nalt),
                   altDepth = tally[!is.ref],
                   refDepth = Rle(tally[is.ref], per.samp.pos),
                   totalDepth = Rle(colSums(tally), per.samp.pos),
@@ -45,3 +47,50 @@ pileupVariants <- function(bams, genome, param = PileupParam(),
   gr <- do.call(c, applyPileups(pileupFiles, pileupFun, param=param))
   gr
 }
+
+### Some unexported work on a converter from a VRanges to a "pileup" GRanges:
+pileupGRanges <- function(x) {
+  indel <- isIndel(x)
+  if (any(indel, na.rm=TRUE)) {
+    x <- x[-which(indel)]
+  }
+  gr <- GRanges(seqnames(x), ranges(x), strand(x))
+  sm <- selfmatch(gr)
+  uniq <- sm == seq_len(length(gr))
+  map <- integer()
+  map[sm[uniq]] <- seq_len(sum(uniq))
+  pos <- map[sm]
+  base <- match(c(alt(x), ref(x)), DNA_BASES)
+  depth <- c(altDepth(x), refDepth(x))
+  samp <- as.factor(sampleNames(x))
+  pileup <- array(0L, c(sum(uniq), length(levels(samp)), length(DNA_BASES)),
+                  dimnames=list(NULL, levels(samp), base=DNA_BASES))
+  pileup[cbind(pos, samp, base)[!is.na(base),]] <- depth[!is.na(base)]
+  ugr <- gr[uniq]
+  ugr$ref[pos] <- ref(x)
+  ugr$pileup <- pileup
+  ugr
+}
+
+pileupGRangesOverGenome <- function(x) {
+  indel <- isIndel(x)
+  if (any(indel, na.rm=TRUE)) {
+    x <- x[-which(indel)]
+  }
+  genome <- unlist(tileGenome(seqinfo(x), tilewidth=1L))
+  gr <- GRanges(seqnames(x), ranges(x), "*")
+  m <- match(gr, genome)
+  base <- match(c(alt(x), ref(x)), DNA_BASES)
+  depth <- c(altDepth(x), refDepth(x))
+  samp <- as.factor(sampleNames(x))
+  pileup <- array(0L, c(length(genome), length(levels(samp)), length(DNA_BASES)),
+                  dimnames=list(NULL, levels(samp), base=DNA_BASES))
+  pileup[cbind(m, samp, base)[!is.na(base),]] <- depth[!is.na(base)]
+  genome$REF <- "."
+  genome$REF[m] <- ref(x)
+  genome$REF <- DNAStringSet(genome$REF)
+  genome$pileup <- pileup
+  genome
+}
+
+
