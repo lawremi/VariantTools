c3b1f2e304b4abf0f3df103684de58498ef108fc
Michael Lawrence <lawremi@iastate.edu>
2017-04-19 23:08:57 +0000

    attempt to break dependency of examples and vignette on gmapR
    also fix some R CMD check issues
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/VariantTools@128911 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/fisher_p.R
+++ R/fisher_p.R
@@ -27,35 +27,16 @@ fisher_p_vectorized <- function(x, y, m, n, relErr = 1 + 1e-7) {
   k <- x + y
   lo <- pmax(0L, k - n)
   hi <- pmin(k, m)
-  support <- S4Vectors:::mseq(lo, hi)
+  support <- as.integer(IRanges(lo, hi))
   w <- hi - lo + 1L
   m <- rep(m, w)
   n <- rep(n, w)
   k <- rep(k, w)
   d <- dhyper(support, m, n, k, log = TRUE)
   part <- PartitioningByWidth(w)
-  group <- togroup(part)
-  maxd <- viewMaxs(Views(Rle(d), part))
-  ##maxd <- d[order(group, d)[end(part)]]
-  d <- exp(d - rep(maxd, w))
-  d <- d / rep(rowsum(d, group, reorder = FALSE), w)
-  sel <- d <= rep(d[x - lo + start(part)], w) * relErr
-  unname(rowsum(d[sel], group[sel], reorder = FALSE)[,1])
-}
-
-## Unused attempt using IRanges Lists. Not much of a speed improvement.
-fisher_p_List <- function( x, y, m, n, relErr = 1 + 1e-7 ) {
-  k <- x + y
-  lo <- pmax(0L, k - n)
-  hi <- pmin(k, m)
-  ranges <- IRanges(lo, hi)
-  support <- as.integer(ranges)
-  m <- rep(m, width(ranges))
-  n <- rep(n, width(ranges))
-  k <- rep(k, width(ranges))
-  d <- relist(Rle(dhyper(support, m, n, k, log = TRUE)),
-              PartitioningByWidth(width(ranges)))
-  d <- exp(d - as(max(d), "List"))
-  d <- d / as(sum(d), "List")
-  sum(d[d <= d[as(x - lo + 1L, "List")] * relErr])
+  dl <- relist(d, part)  
+  maxd <- max(dl)
+  dl <- exp(dl - maxd) / sum(dl)
+  sel <- dl <= dl[as(x - lo + 1L, "IntegerList")] * relErr
+  unname(sum(dl[sel]))
 }
